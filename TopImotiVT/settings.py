ce20055d8e65f7c38f7361122077f6f337ca9fed
TatyanaManolova <shuralevatv@mail.ru>
2025-04-29 19:12:45 +0300

    correct allowed hosts 2

--- TopImotiVT/settings.py
+++ TopImotiVT/settings.py
@@ -24,7 +24,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 if DEBUG:
     ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 else:
-    ALLOWED_HOSTS = ["topimotivt.onrender.com", ".topimotivt.onrender.com"]
+    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 INSTALLED_APPS = [
